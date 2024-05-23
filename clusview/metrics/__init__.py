7090ca55170478146520f29c5237c4bb4548918e
Germán Calcedo <49457798+gcalcedo@users.noreply.github.com>
2024-05-23 11:45:39 +0200

    feat: add silhouette score metric component (#18)

---
+++ clusview/metrics/__init__.py
@@ -0,0 +1,3 @@
+"""
+Topic modelling performance metrics.
+"""
---
+++ clusview/metrics/silhouette_score.py
@@ -0,0 +1,24 @@
+from bertopic import BERTopic
+import numpy as np
+from .metric_combinator import BaseMetric
+from sklearn.metrics import silhouette_score
+
+
+class SilhouetteScore(BaseMetric):
+    """
+    Silhouette Score is obtained by averaging across the silhouette coefficient of each sample.
+    The Silhouette Coefficient is defined for each sample and is composed of two scores:
+
+    - A: The mean distance between a sample and all other points in the same class.
+    - B: The mean distance between a sample and all other points in the next nearest cluster.
+    """
+
+    def perform_metric(self, topic_model: BERTopic, embeddings: np.ndarray) -> float:
+        reduced_embeddings = topic_model.umap_model.transform(embeddings)
+        topics = topic_model.topics_
+        indices = [index for index, topic in enumerate(topics) if topic != -1]
+
+        X = reduced_embeddings[np.array(indices)]
+        labels = [topic for index, topic in enumerate(topics) if topic != -1]
+        
+        return silhouette_score(X, labels)
