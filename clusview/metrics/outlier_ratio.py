f3cdc83bbeba8129deb3abcf291e24fe67501bbf
gcalcedo <devcalcedo@gmail.com>
2024-05-25 09:31:01 +0200

    Format metric files

--- clusview/metrics/outlier_ratio.py
+++ clusview/metrics/outlier_ratio.py
@@ -11,10 +11,10 @@ class OutlierRatio(BaseMetric):
     def perform_metric(self, topic_model: BERTopic, embeddings: ndarray) -> float:
         topic_info = topic_model.get_topic_info()
         outlier_info = topic_info.loc[0]
-        
+
         outlier_count = outlier_info["Count"] if outlier_info["Topic"] == -1 else 0
         total_count = topic_info["Count"].sum()
-        
+
         outlier_ratio = outlier_count / total_count
-        
+
         return outlier_ratio
