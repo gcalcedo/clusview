02a9b1d07fdeaf88c74f4f72d93d940aa01a44f0
gcalcedo <devcalcedo@gmail.com>
2024-05-25 09:13:51 +0200

    Add topic counter metric

---
+++ clusview/metrics/topic_count.py
@@ -0,0 +1,11 @@
+from bertopic import BERTopic
+from numpy import ndarray
+from clusview.metrics.metric_combinators import BaseMetric
+
+
+class TopicCount(BaseMetric):
+    """
+    Topic Count is the number of topics generated, including the outliers topic.
+    """
+    def perform_metric(self, topic_model: BERTopic, embeddings: ndarray) -> float:
+        return len(topic_model.get_topic_info().index)
