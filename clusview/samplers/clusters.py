8d225c93425af1d86b8deb4b8c5b0a9278096b5c
gcalcedo <devcalcedo@gmail.com>
2024-06-03 10:58:11 +0200

    Add polynomial sampler

---
+++ clusview/samplers/clusters.py
@@ -0,0 +1,31 @@
+from itertools import product
+from typing import List
+from hdbscan import HDBSCAN
+
+from clusview.samplers.parameter_samplers import BaseSampler
+
+
+class HDBSCANSampler:
+    """
+    Iterates over HDSBCAN configurations based on a complete set of combinations
+    for a given list of parameter samplers.
+
+    ### Args
+    - `parameter_samplers : List[ParameterRange]`: list of parameter ranges.
+    """
+
+    def __init__(self, parameter_samplers: List[BaseSampler]) -> None:
+        self.parameter_samplers = parameter_samplers
+
+    def generate_combinations(self):
+        samplings = [sampler.sample_range() for sampler in self.parameter_samplers]
+
+        for combination in product(*samplings):
+            yield combination
+
+    def iterate_configurations(self):
+        for combination in self.generate_combinations():
+            hdbscan = HDBSCAN()
+            for config, value in zip(self.parameter_samplers, combination):
+                setattr(hdbscan, config.parameter_name, value)
+            yield hdbscan
