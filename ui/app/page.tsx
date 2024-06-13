63587c486fad7039fbe57dfb28add924111f0da0
Germán Calcedo <49457798+gcalcedo@users.noreply.github.com>
2024-06-14 00:46:58 +0200

    feat: add ui project template (#53)

---
+++ ui/app/page.tsx
@@ -0,0 +1,17 @@
+export default function Home() {
+  return (
+    <main
+      className="h-screen text-white w-screen bg-[#181825]
+      flex justify-center items-center"
+    >
+      <div
+        className="w-96 h-96 flex flex-col justify-center items-center space-y-10
+        rounded-md border-[#585b70] border-2 bg-[#1e1e2e]"
+      >
+        <h1 className="text-4xl font-bold">ClusView UI</h1>
+        <h1 className="text-xl">Under development</h1>
+        <h1 className="text-9xl">🚧</h1>
+      </div>
+    </main>
+  );
+}
