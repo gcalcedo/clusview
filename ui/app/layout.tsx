87089ed1340869192f5dcad1b2ba5739cd74ead5
gcalcedo <devcalcedo@gmail.com>
2024-06-14 00:45:19 +0200

    Add ui project template

---
+++ ui/app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import { Poppins } from "next/font/google";
+import "./globals.css";
+
+const poppins = Poppins({ subsets: ["latin"], weight: ["400", "700"] });
+
+export const metadata: Metadata = {
+  title: "ClusView",
+  description: "ClusView User Interface",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={poppins.className}>{children}</body>
+    </html>
+  );
+}
