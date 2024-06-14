8975df969d46c65e7de4e204f83c625e1e338136
Germán Calcedo <49457798+gcalcedo@users.noreply.github.com>
2024-06-14 11:44:52 +0200

    feat: catppuccin theme (#63) #patch

--- packages/ui/app/page.tsx
+++ packages/ui/app/page.tsx
@@ -1,12 +1,12 @@
 export default function Home() {
   return (
     <main
-      className="h-screen text-white w-screen bg-[#181825]
+      className="h-screen text-ctp-text w-screen bg-ctp-crust
       flex justify-center items-center"
     >
       <div
         className="w-96 h-96 flex flex-col justify-center items-center space-y-10
-        rounded-md border-[#585b70] border-2 bg-[#1e1e2e]"
+        rounded-md border-ctp-surface2 border-2 bg-ctp-mantle"
       >
         <h1 className="text-4xl font-bold">ClusView UI</h1>
         <h1 className="text-xl">Under development</h1>
