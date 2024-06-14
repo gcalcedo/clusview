44d715be70ed7269c46ae05a88e6db7d17d7cc78
gcalcedo <devcalcedo@gmail.com>
2024-06-14 15:21:37 +0200

    Improve samplers docs

--- packages/clusview/samplers/parameters/base_parameter_sampler.py
+++ packages/clusview/samplers/parameters/base_parameter_sampler.py
@@ -5,6 +5,11 @@ from typing import Iterable
 class BaseSampler(ABC):
     """
     Base class for all parameter samplers.
+
+    Attributes:
+        parameter_name (str): The name of the parameter being sampled.
+        lower_bound (int): The lower bound of the parameter's range.
+        upper_bound (int): The upper bound of the parameter's range.
     """
 
     def __init__(self, parameter_name: str, lower_bound: int, upper_bound: int):
@@ -17,4 +22,10 @@ class BaseSampler(ABC):
         pass
 
     def get_sampling_count(self) -> int:
+        """
+        Returns the number of samples in the parameter's range.
+
+        Returns:
+            int: The number of samples in the parameter's range.
+        """
         return len(self.sample_range())
--- packages/clusview/samplers/parameters/linear_sampler.py
+++ packages/clusview/samplers/parameters/linear_sampler.py
@@ -5,7 +5,21 @@ from samplers.parameters.base_parameter_sampler import BaseSampler
 
 class LinearSampler(BaseSampler):
     """
-    Linear parameter sampler.
+    A sampler that generates linearly spaced values within a given range.
+
+    Args:
+        parameter_name (str): The name of the parameter being sampled.
+        lower_bound (int): The lower bound of the parameter range (inclusive).
+        upper_bound (int): The upper bound of the parameter range (inclusive).
+        step (int): The step size between consecutive values.
+
+    Returns:
+        Iterable: An iterable containing the linearly spaced values within the range.
+
+    Examples:
+        >>> sampler = LinearSampler('parameter_name', 0, 10, 2)
+        >>> sampler.sample_range()
+        [0, 2, 4, 6, 8, 10]
     """
 
     def __init__(
