a7b3b49e7be8e117e5ba1e900e9040e5a781b471
Germán Calcedo <49457798+gcalcedo@users.noreply.github.com>
2024-06-20 15:49:13 +0200

    feat: react flow integration (#92)

---
+++ packages/ui/src/app/page.tsx
@@ -0,0 +1,9 @@
+import ClusviewFlow from "../components/ClusviewFlow";
+
+export default function Home() {
+  return (
+    <main className="w-screen h-screen bg-ctp-base">
+      <ClusviewFlow />
+    </main>
+  );
+}
--- packages/ui/tailwind.config.ts
+++ packages/ui/tailwind.config.ts
@@ -2,14 +2,13 @@ import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   plugins: [
     require("@catppuccin/tailwindcss")({
       prefix: "ctp",
-      defaultFlavour: "macchiato",
+      defaultFlavour: "mocha",
     }),
   ],
 };
