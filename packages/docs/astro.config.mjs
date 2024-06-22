4f2874b30015355e11d9358b0d4181f49500c190
gcalcedo <devcalcedo@gmail.com>
2024-06-22 20:50:22 +0200

    Update base

--- packages/docs/astro.config.mjs
+++ packages/docs/astro.config.mjs
@@ -4,7 +4,7 @@ import starlight from "@astrojs/starlight";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://gcalcedo.github.io',
-  base: 'clusview/packages/docs/',
+  base: '/clusview/packages/docs/',
   integrations: [
     starlight({
       title: "Clusview",
