ecf4d4f4dc2f527c39bd1b38031f1aa08757c81d
gcalcedo <devcalcedo@gmail.com>
2024-06-22 16:56:07 +0200

    Add custom Tailwind theme to ui

--- packages/ui/tailwind.config.ts
+++ packages/ui/tailwind.config.ts
@@ -5,5 +5,22 @@ const config: Config = {
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+  theme: {
+    extend: {
+      colors: {
+        green: "#36E7A7",
+        blue: "#33B6FF",
+        red: "#36E7A7",
+        yellow: "#36E7A7",
+        white: "#E3E7F5",
+        black: "#1A1C27",
+        "gray-5": "#252D3E",
+        "gray-4": "#30384B",
+        "gray-3": "#4D5B7A",
+        "gray-2": "#65769C",
+        "gray-1": "#9AA9CB"
+      },
+    },
+  },
 };
 export default config;
