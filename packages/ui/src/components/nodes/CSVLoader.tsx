220c59d4b73bc769fcd4b99a22c3591b50b791b0
Germán Calcedo <49457798+gcalcedo@users.noreply.github.com>
2024-06-22 19:46:29 +0200

    feat: integrate custom theme (#97)

--- packages/ui/src/components/nodes/CSVLoader.tsx
+++ packages/ui/src/components/nodes/CSVLoader.tsx
@@ -3,13 +3,17 @@ import { IconTablePlus } from "@tabler/icons-react";
 
 export default function CSVLoader() {
   return (
-    <div className="bg-[#252D3E] border-[#4d5b7a] hover:border-[#36E7A7] border rounded group">
+    <div className="bg-gray-5 border-gray-3 hover:border-green border rounded group">
       <div className="flex space-x-2 p-3 items-center relative h-10">
-        <IconTablePlus className="text-[#65769c] group-hover:text-[#36E7A7]" size={20} stroke={1.5} />
-        <div className="text-[#36E7A7]">CSV Loader</div>
+        <IconTablePlus
+          className="text-gray-2 group-hover:text-green"
+          size={20}
+          stroke={1.5}
+        />
+        <div className="text-green">CSV Loader</div>
         <Handle
           type="source"
-          className="!rounded-sm hover:outline-[#36E7A7] hover:!bg-[#E3E7F5] !w-2 !h-2 !bg-[#36E7A7] !mx-[-1rem] outline-offset-2 hover:outline !border-none"
+          className="!rounded-sm hover:outline-green hover:!bg-white !w-2 !h-2 !bg-green !mx-[-1rem] outline-offset-2 hover:outline !border-none"
           position={Position.Right}
         />
       </div>
--- packages/ui/tailwind.config.ts
+++ packages/ui/tailwind.config.ts
@@ -5,11 +5,22 @@ const config: Config = {
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-  plugins: [
-    require("@catppuccin/tailwindcss")({
-      prefix: "ctp",
-      defaultFlavour: "mocha",
-    }),
-  ],
+  theme: {
+    extend: {
+      colors: {
+        green: "#36E7A7",
+        blue: "#33B6FF",
+        red: "#EE436C",
+        yellow: "#F8D762",
+        white: "#E3E7F5",
+        black: "#1A1C27",
+        "gray-5": "#252D3E",
+        "gray-4": "#30384B",
+        "gray-3": "#4D5B7A",
+        "gray-2": "#65769C",
+        "gray-1": "#9AA9CB"
+      },
+    },
+  },
 };
 export default config;
