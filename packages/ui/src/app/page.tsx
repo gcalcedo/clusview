e45206a7250922354c1b57aebde097af3147366c
Germán Calcedo <49457798+gcalcedo@users.noreply.github.com>
2024-06-22 16:23:18 +0200

    feat: add csv loader node (#95)

--- packages/ui/src/app/page.tsx
+++ packages/ui/src/app/page.tsx
@@ -2,7 +2,7 @@ import ClusviewFlow from "../components/ClusviewFlow";
 
 export default function Home() {
   return (
-    <main className="w-screen h-screen bg-ctp-base">
+    <main className="w-screen h-screen bg-[#191C28]">
       <ClusviewFlow />
     </main>
   );
