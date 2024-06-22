caa08e33fc727ebbe9687fc1d4e1af8cd9a7f910
gcalcedo <devcalcedo@gmail.com>
2024-06-22 19:07:12 +0200

    Add black theme color as background

--- packages/ui/src/app/page.tsx
+++ packages/ui/src/app/page.tsx
@@ -2,7 +2,7 @@ import ClusviewFlow from "../components/ClusviewFlow";
 
 export default function Home() {
   return (
-    <main className="w-screen h-screen bg-[#191C28]">
+    <main className="w-screen h-screen bg-black">
       <ClusviewFlow />
     </main>
   );
