e1a2decd17803ba07e864c1ab5a38ed465997d04
Germán Calcedo <49457798+gcalcedo@users.noreply.github.com>
2024-07-03 23:40:22 +0200

    feat: add option to fix UMAP seed (#115)

--- packages/clusview/pipelines/metric_mapper.py
+++ packages/clusview/pipelines/metric_mapper.py
@@ -23,12 +23,14 @@ class MetricMapper:
         hdbscan_sampler: HDBSCANSampler,
         metrics: Dict[str, BaseMetric],
         runs: int = 1,
+        umap_seed: int = None,
     ) -> None:
         self.document_loader = document_loader
         self.transformer = transformer
         self.hdbscan_sampler = hdbscan_sampler
         self.metrics = metrics
         self.runs = runs
+        self.umap_seed = umap_seed
 
     def cluster(self, hdbscan: HDBSCAN, embeddings: np.ndarray):
         clusters = hdbscan.fit_predict(embeddings)
@@ -69,11 +71,17 @@ class MetricMapper:
 
         with ProcessPoolExecutor(max_workers=cpu_count()) as pool:
             for _ in range(self.runs):
+                if self.umap_seed is not None:
+                    random_state = self.umap_seed
+                else:
+                    random_state = np.random.randint(1, 2**32)
+                
                 reduced_embeddings = UMAP(
                     n_neighbors=15,
                     n_components=5,
                     min_dist=0.0,
                     metric="cosine",
+                    random_state=random_state
                 ).fit_transform(embeddings)
 
                 for hdbscan in self.hdbscan_sampler.iterate_configurations():
