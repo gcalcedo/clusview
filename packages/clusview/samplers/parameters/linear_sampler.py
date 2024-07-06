cf70dee5dfc8912fd3f9a90e6cee995bfc2f595f
gcalcedo <devcalcedo@gmail.com>
2024-07-06 12:42:32 +0200

    Define LinearSampler with number of samples

--- packages/clusview/samplers/parameters/linear_sampler.py
+++ packages/clusview/samplers/parameters/linear_sampler.py
@@ -1,32 +1,39 @@
 from typing import Iterable
 
+import numpy as np
+
 from .base_parameter_sampler import BaseSampler
 
 
 class LinearSampler(BaseSampler):
     """
-    A sampler that generates linearly spaced values within a given range.
+    Generates linearly spaced values within a given range.
 
     Args:
         parameter_name (str): The name of the parameter being sampled.
         lower_bound (int): The lower bound of the parameter range (inclusive).
         upper_bound (int): The upper bound of the parameter range (inclusive).
-        step (int): The step size between consecutive values.
+        number_of_samples (int): The number of samples to generate.
 
     Returns:
         Iterable: An iterable containing the linearly spaced values within the range.
 
     Examples:
-        >>> sampler = LinearSampler('parameter_name', 0, 10, 2)
+        >>> sampler = LinearSampler('x', 0, 10, 6)
         >>> sampler.sample_range()
         [0, 2, 4, 6, 8, 10]
     """
 
     def __init__(
-        self, parameter_name: str, lower_bound: int, upper_bound: int, step: int
+        self,
+        parameter_name: str,
+        lower_bound: int,
+        upper_bound: int,
+        number_of_samples: int,
     ):
-        super().__init__(parameter_name, lower_bound, upper_bound)
-        self.step = step
+        super().__init__(parameter_name, lower_bound, upper_bound, number_of_samples)
 
     def sample_range(self) -> Iterable:
-        return range(self.lower_bound, self.upper_bound + 1, self.step)
+        return np.linspace(
+            self.lower_bound, self.upper_bound, self.number_of_samples, dtype=int
+        )
