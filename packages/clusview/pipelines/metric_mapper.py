da2c43c31a2da5b6e6dee7c5ecb841b735ded12f
Germán Calcedo <49457798+gcalcedo@users.noreply.github.com>
2024-07-06 12:46:54 +0200

    feat: allow for custom out directory in MetricMapper (#119)

--- packages/clusview/pipelines/metric_mapper.py
+++ packages/clusview/pipelines/metric_mapper.py
@@ -24,6 +24,7 @@ class MetricMapper:
         metrics: Dict[str, BaseMetric],
         runs: int = 1,
         umap_seed: int = None,
+        out_dir: str = "default",
     ) -> None:
         self.document_loader = document_loader
         self.transformer = transformer
@@ -31,6 +32,7 @@ class MetricMapper:
         self.metrics = metrics
         self.runs = runs
         self.umap_seed = umap_seed
+        self.out_dir = out_dir
 
     def cluster(self, hdbscan: HDBSCAN, embeddings: np.ndarray):
         clusters = hdbscan.fit_predict(embeddings)
@@ -75,13 +77,13 @@ class MetricMapper:
                     random_state = self.umap_seed
                 else:
                     random_state = np.random.randint(1, 2**32)
-                
+
                 reduced_embeddings = UMAP(
                     n_neighbors=15,
                     n_components=5,
                     min_dist=0.0,
                     metric="cosine",
-                    random_state=random_state
+                    random_state=random_state,
                 ).fit_transform(embeddings)
 
                 for hdbscan in self.hdbscan_sampler.iterate_configurations():
@@ -100,7 +102,7 @@ class MetricMapper:
                 {metric: "mean"}
             )
             sorted_df = combined_df.sort_values(by=sampler_names)
-            sorted_df.to_csv(f"result/{metric}_map.csv", index=False)
+            sorted_df.to_csv(f"{self.out_dir}/{metric}_map.csv", index=False)
 
         progress_bar.close()
         pool.shutdown(wait=True)
